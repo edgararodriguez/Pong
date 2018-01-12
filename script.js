7c2a92f9baa93ce1054d4f49b0861085bd48ac10
Caleb Mahala <31549512+calebgmahala@users.noreply.github.com>
2018-01-11 22:58:02 -0500

    stuff

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
+var shake1 = 0;
+var shake2 = 0;
 function follow() {
 	var pointer = document.getElementById('pointer');
-	var x = event.clientX;
-    var y = event.clientY;
-    pointer.style.left = x;
-    pointer.style.top = y;
+	var x = event.clientX + (Math.random()*shake1);
+    var y = event.clientY + (Math.random()*shake1);
+    pointer.style.left = x +'px';
+    pointer.style.top = y +'px';
+    console.log(x)
+}
+function bounce() {
+	var pointer = document.getElementById('pointer');
+	var ball = document.getElementById('ball');
+	var dif = pointer.getBoundingClientRect().left - ball.getBoundingClientRect().left;
+	return (dif*1.6)
+}
+function start() {
+	console.log(bounce());
+}
+function remove(cup, player) {
+	document.getElementById(cup).classList.add("hide");
+	if(player === 1) {
+		shake1 = shake1 + 5;
+	} else {
+		shake2 = shake2 + 5;
+	}
 }
